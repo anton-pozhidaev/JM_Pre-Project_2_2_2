0df32345c67f50efa0b26f502e31ee0d78a7c6b8
anton-pozhidaev <anton.pozhidaev@gmail.com>
2021-01-11 20:32:18 +0300

    all works! y-aa-hooooo !

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,12 +1,12 @@
 package web.controller;
 
-import model.Car;
+import web.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import service.CarService;
+import web.service.CarService;
 
 import java.util.List;
 
